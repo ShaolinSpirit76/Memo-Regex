95d0fda3341871949ef5fe38758927554252de87
ShaolinSpirit76 <paul.compere76@gmail.com>
2019-05-28 14:31:46 +0200

    correction phone + address

--- regex.js
+++ regex.js
@@ -35,7 +35,7 @@ var frenchDate = event.toLocaleDateString("fr-FR", options);
 Pour les calendrier :
 
 // Prends en compte les années bisextiles.
-regexCalendar : ^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$
+regexCalendar : ^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\g1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\g2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\g3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\g4(?:(?:1[6-9]|[2-9]\d)?\d{2})$
 
 
 Pour le numéro de téléphone, en indiquant le +33 dans votre label, remplaçant ainsi le premier 0 (numéro international) :
@@ -44,21 +44,24 @@ Pour le numéro de téléphone, en indiquant le +33 dans votre label, remplaçan
 regexPhone = /^[0-9]{9}$/;
 
 // 2) Avec la possibilité de séparer par un point, un espace ou un tiret, ou en les collant, ou en ajoutant "+33" à la place du zéro.
-regexPhone = /^([0][1-9]([-. ]?[0-9]{2}){4})|([+][3][3][1-9]([-. ]?[0-9]{2}){4})$/;
+regexPhone = /^[0][1-9]([-. ]?)(([0-9]{2})\1([0-9]{2}))(\1([0-9]{2})){2}|[+]33[0-9]([-. ]?)(([0-9]{2})\7([0-9]{2}))(\7([0-9]{2})){2}$/;
 
 
 Pour récupérer une adresse postale :
 
 // 1) Le nom de la rue :
-regexStreet = /^[a-zA-ZéèÉÈôîêûÛÊÔÎùÙïöëüËÏÖÜ' ]{2,15}[- ']?[a-zA-ZéèÉÈôîêûÛÊÔÎùÙïöëüËÏÖÜ ]{0,15}[a-zA-ZéèÉÈôîêûÛÊÔÎùÙïöëüËÏÖÜ ]{0,15}$/;
+regexStreet = /^(avenue|Avenue|AVENUE|rue|Rue|RUE|Boulevard|BOULEVARD|boulevard|Impasse|IMPASSE|impasse|Allée|ALLEE|allée|hameau|Hameau|HAMEAU|Chemin|chemin|CHEMIN|lieu-dit|Lieu-dit|LIEU-DIT|lieu-Dit|Lieu-Dit)[- ][A-Za-zéèàâêŷûîôäëïöüÿù]+([- ]?[A-Za-zéèàâêŷûîôäëïöüÿù]{0,17}){0,3}$/;
 
 // 2) Le numéro de la rue :
-regexStreetNumber = /^[0-9]+$/;
+regexStreetNumber = /^([1-9]|[1-9][0-9]|[1-9][0-9]{2}|1[0-9]{3})[A|a|B|b]?[ ]?(bis)?$/;
 
-// 3) Le code postal correspondant au territoire français (métropole + DOM-TOM)
+// 3) numéro de rue + nom de rue :
+regexAddress = /^([1-9]|[1-9][0-9]|[1-9][0-9]{2}|1[0-9]{3})[A|a|B|b]?[ ]?(bis)?[- ](avenue|Avenue|AVENUE|rue|Rue|RUE|Boulevard|BOULEVARD|boulevard|Impasse|IMPASSE|impasse|Allée|ALLEE|allée|hameau|Hameau|HAMEAU|Chemin|chemin|CHEMIN|lieu-dit|Lieu-dit|LIEU-DIT|lieu-Dit|Lieu-Dit)[- ][A-Za-zéèàâêŷûîôäëïöüÿù]+([- ]?[A-Za-zéèàâêŷûîôäëïöüÿù]{0,17}){0,3}$/;
+
+// 4) Le code postal correspondant au territoire français (métropole + DOM-TOM)
 regexPostal = /^[0-9]{5}|[9]{1}[7]{1}[1-6]{1}$/;
 
-// 4) La première regexName fonctionne très bien pour le nom de la ville.
+// 5) La première regexName fonctionne très bien pour le nom de la ville.
 regexCity = /^[a-zA-ZéèÉÈôîêûÛÊÔÎùÙïöëüËÏÖÜ']{2,17}[- ']?[a-zA-ZéèÉÈôîêûÛÊÔÎùÙïöëüËÏÖÜ]{0,17}$/;
 
 Pour le login/identifiant :
