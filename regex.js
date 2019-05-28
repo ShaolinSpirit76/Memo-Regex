736a45bbd9a6b4c1f6518a8e19a76de5d104a239
ShaolinSpirit76 <paul.compere76@gmail.com>
2019-05-28 09:23:42 +0200

    regexCalendar

--- regex.js
+++ regex.js
@@ -32,6 +32,11 @@ var event = new Date(date);
 var options = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
 var frenchDate = event.toLocaleDateString("fr-FR", options);
 
+Pour les calendrier :
+
+// Prends en compte les années bisextiles.
+regexCalendar : ^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$
+
 
 Pour le numéro de téléphone, en indiquant le +33 dans votre label, remplaçant ainsi le premier 0 (numéro international) :
 
